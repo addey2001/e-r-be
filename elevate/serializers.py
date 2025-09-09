35a1706e1a78a434743f735483d36382e6f1599f
addey2001 <addey2001@gmail.com>
2025-09-09 23:53:42 +0100

    finished project

--- elevate/serializers.py
+++ elevate/serializers.py
@@ -3,14 +3,7 @@ from .models import Listing
 
 class ListingSerializer(serializers.ModelSerializer):
     owner = serializers.StringRelatedField()
-    is_favorite = serializers.SerializerMethodField()
 
     class Meta:
         model = Listing
-        fields = ['id', 'title', 'description', 'price', 'owner', 'is_favorite']
-
-        def get_is_favorite(self, obj):
-            user = self.context['request'].user
-            if user.is_authenticated:
-                return obj.favorites.filter(id=user.id).exists()
-            return False
+        fields = '__all__'
